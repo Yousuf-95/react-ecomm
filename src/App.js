de7af700ed67dc50ca13430e45147fd3e2aca423
Yousuf <yf.yousuf95@gmail.com>
2020-03-14 03:33:32 +0530

    Added a navigation bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route} from 'react-router-dom';
 import './App.css';
 import  HomePage  from './Pages/Homepage/homepage-component';
 import  ShopPage from './Pages/Shop/shop-component.jsx';
+import Header from './Components/header-component';
 
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
